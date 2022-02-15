2689a8956a0c0d6f9e5f2c9bea7c5554e872bd56
Romain <romain.reghem@etud.univ-jfc.fr>
2022-02-15 14:43:06 +0100

    "Zones bio" is now on an activable layer, JSON is now hosted online

--- beeo/src/app/_services/map.service.ts
+++ beeo/src/app/_services/map.service.ts
@@ -7,9 +7,11 @@ import * as L from 'leaflet';
 })
 export class MapService {
 
-  baseUrl: string = 'http://localhost:3000/data';
+  baseUrl: string = 'https://api.jsonbin.io/b/620bacc94bf50f4b2dfccb00';
+  greenZones = L.layerGroup();
+  map;
 
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient) {
 
   }
 
@@ -17,24 +19,30 @@ export class MapService {
     return {
       fillColor: "#25633D",
       opacity: 0.8,
-      weight:1,
+      weight: 1,
       color: 'white',
       fillOpacity: 0.7
     };
   }
 
-  drawPolygons(map: L.Map): void{
-    const marker = L.marker([42.949162366328906, 2.563953491777443]).addTo(map);
+  drawPolygons(map: L.Map): void {    
+    this.map = map;
+    const marker = L.marker([42.949162366328906, 2.563953491777443]).addTo(map);    
     this.http.get(this.baseUrl).subscribe((res: any) => {
-      for(const c of res){
+      for (const c of res.data) {
         //let polygon = L.GeoJSON.geometryToLayer(c.geometry).addTo(map);
-        let polygon = L.geoJSON(c.geometry,{style:this.style}).addTo(map);
+        let polygon = L.geoJSON(c.geometry, { style: this.style }).addTo(this.greenZones);
         console.log(c.properties.LBL_CULTU);
-
-        polygon.bindPopup("<center><h1>Exploitation biologique</h1><img style='width:100%;'src='https://i.imgur.com/2X88trj.jpeg'/><p>Type de culture :"+ c.properties.LBL_CULTU + "</p><p>Surface : " + c.properties.SURFACE_HA + " hectares</p></center>");
-        
+        polygon.bindPopup("<center><h1>Exploitation biologique</h1><img style='width:100%;'src='https://i.imgur.com/2X88trj.jpeg'/><p>Type de culture :" + c.properties.LBL_CULTU + "</p><p>Surface : " + c.properties.SURFACE_HA + " hectares</p></center>");
       }
-
     });
+  }
+
+  greenZonesDisplay():void{
+    this.greenZones.addTo(this.map);
+  }
+
+  greenZonesHide():void{
+    this.greenZones.removeFrom(this.map);
   }
 }
--- beeo/src/app/sidebar/sidebar.component.ts
+++ beeo/src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MapService } from '../_services/map.service';
+import { ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,6 +9,12 @@ import { Component } from '@angular/core';
 })
 export class SidebarComponent {
 
-  
+  constructor(private mapService: MapService){}
+  @ViewChild('layer1') layer1: ElementRef | undefined;
+  layer1check():void {
+    if(this.layer1?.nativeElement.checked) this.mapService.greenZonesDisplay();    
+    else this.mapService.greenZonesHide();
+  }
+
 
 }
